0bafe979461a6a57572f0e33cd64a3c7a922ea1d
m4rcous <m.gongora.sanchez08@gmail.com>
2025-01-14 12:26:05 -0500

    feat: learn about if-else_if-else sentence

--- src/main/java/SentenciaIf.java
+++ src/main/java/SentenciaIf.java
@@ -6,6 +6,9 @@ public class SentenciaIf {
         if (edad >= 18) {
             System.out.println("Eres mayor de edad");
         }
+        else if (edad >= 13 && edad < 18) {
+            System.out.println("Eres un adolescente");
+        }
         else {
             System.out.println("Eres menor de edad");
         }
